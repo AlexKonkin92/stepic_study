45c2fc4d2ac671430c9666e59a7849c04f5aa97a
AlexKonkin92 <konkin.alex777@mail.ru>
2020-03-19 19:58:06 +0300

    Добавил комменты

--- Строки/Вывод символа и его кол-ва в строке.py
+++ Строки/Вывод символа и его кол-ва в строке.py
@@ -5,16 +5,16 @@ b = 0
 g = 0
 i = 0
 while i != j:
-    if i != p:
-        if s[i] == s[i+1]:
+    if i != p:#до предпоследнего символа
+        if s[i] == s[i+1]:#проверяем с последующим символом
                     g = g + 1
                     i = i + 1
         else:
-                    print(s[i],g+1,sep='',end='')
+                    print(s[i],g+1,sep='',end='')#если не совпадает, то выводим
                     i = i + 1
                     g = 0
     else:
-                if s[i] == s[i]:
+                if s[i] == s[i]:#сравниваем с последним символом
                     g = g + 1
                     print(s[i], g, sep='', end='')
                     break
