c66f688f511ca7132c123cb2a354eaefe4c3fb52
AlexKonkin92 <konkin.alex777@mail.ru>
2020-04-01 23:55:07 +0300

    Раздел файловый ввод_вывод

---
+++ Файловый ввод_вывод/Вывод часто встречающего слова в тексте.py
@@ -0,0 +1,21 @@
+a = 'ABc a abc'
+#inf =  open (r'C:\Users\79851\PycharmProjects\Alex\venv\GitHub\stepic_study\dataset_3363_3.txt','r')
+#a =  inf.read().replace('\n', ' ').lower().split()
+#inf.close()
+#a.sort()
+b = {}
+c = []
+g = 0
+for i in a:
+  if i not in b.keys():
+    b[i] = len(i)
+g = max(b.values())
+for key, value in b.items():
+    if value == g:
+        c.append(key)
+        c.append(value)
+        break
+print(c)
+#with  open (r'C:\Users\79851\PycharmProjects\Alex\venv\GitHub\stepic_study\file.txt','w') as ouf:
+#    ouf.write(c[0] + ' ' + str(c[1]))
+#    ouf.close()
